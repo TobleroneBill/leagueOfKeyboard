1718a96c807edf5c6d1e4b4ae9d23b0906fa3311
TobleroneBill <101929340+TobleroneBill@users.noreply.github.com>
2023-02-27 16:22:00 +0000

    cool autogui cheats
    not 100% reliable, but works well enough.
    Big issues are how slow this is, and the angle of camera

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import pyautogui
 import keyboard
 
-
+# TODO: This will be targeted towards MELEE champs, so need an array of 8 positions (maybe 9 if we include center)
+# That can be used to reset the positions when a user gets out of position with AutoAiming
 
 centerx, centery = pyautogui.size().width/2, pyautogui.size().height/2
 
-
+enemyTestR = 'RedTest.png'
 
 movementSizeMax = centerx/2.5
 movementSizestep = movementSizeMax/2
@@ -14,20 +15,67 @@ movementSize = movementSizeMax
 ADCRange = movementSizeMax
 MeleeRange = movementSizeMax/2
 
-
-
 xRight = centerx + movementSizeMax
 xLeft = centerx - movementSizeMax
 yBot = centery + movementSizeMax
 yTop = centery - movementSizeMax
-
 
 xPos = centerx
 yPos = centery
 
 toggleCenter = False
 meleeADCNormalSwitch = 0
+attackMode = True
 
+#inputKeys = ('q','w','e','r','1','2','3','4','5','6')
+inputKeys = ('q','w','e','r')
+
+def UpdateScreenshot():
+    global screenshot, xPos, yPos, enemyTestR
+    # Mouse x and y, + AimAssist Range len + wid
+    # x and y need to -= aimassist/2 to get this section of the screen
+    rangeSizex = 500         # in Pixels
+    rangeSizey = 600
+    resolution = 2
+    # Looks for health bar colors to target characters
+    minionBar = (202,91,91)
+    champBar = (154,37,27)
+
+    xmin = int(xPos-(rangeSizex/2))
+    xMax = int(rangeSizex)
+
+    # TODO: need a system to dynamically size the Y axis. I think depending on the yPos (if above/below center apply
+    # scalar to the yRange. Find percentage of screen that it is above center, and then yeah
+    ymin = int(yPos-rangeSizey*1.3)
+    yMax = int(rangeSizey * 2)
+
+    sc = pyautogui.screenshot('img.png',region=(xmin,ymin,xMax,yMax))
+
+    for x in range(0,sc.width,resolution):
+        for y in range(0, sc.height, resolution):
+            if sc.getpixel((x,y)) == champBar:
+                xPos = xmin+x
+                yPos = ymin+y
+
+                if x > sc.width/2:
+                    xPos += 50
+                else:
+                    xPos += 25
+                yPos+= 100
+                return
+
+            if sc.getpixel((x,y)) == minionBar:
+                xPos = xmin + x
+                yPos = ymin + y
+
+                if x > sc.width / 2:
+                    xPos += 50
+                else:
+                    xPos += 25
+                yPos += 100
+                return
+
+    print('heyo')
 
 
 def ResetPos():
@@ -35,13 +83,13 @@ def ResetPos():
     xPos = centerx
     yPos = centery
 
-
-
 # Changes the range into 3 states, Melee range,ADC range, and a middleground range that has ADC size but melee step
 def SwapRange():
     print('_____________________________________________________')
     print('swapping')
+    # Don't remember this being a requirement for stuff to work, but OK
     global meleeADCNormalSwitch, movementSizeMax, movementSizestep ,xRight,xLeft,yBot,yTop, centerx
+
     meleeADCNormalSwitch += 1
     if meleeADCNormalSwitch > 2:
         meleeADCNormalSwitch = 0
@@ -98,9 +146,12 @@ def MouseToKey(key):
                     xPos += movementSizestep
                 if xPos > centerx:
                     xPos -= movementSizestep
+            if yPos == yBot:
+                yPos = yTop
             if yPos > yTop:
                 newPos = yPos - movementSizestep
                 yPos = newPos
+
 
 
 
@@ -125,6 +176,8 @@ def MouseToKey(key):
                     xPos += movementSizestep
                 if xPos > centerx:
                     xPos -= movementSizestep
+            if yPos == yTop:
+                yPos = yBot
             if yPos < yBot:
                 newPos = yPos + movementSizestep
                 yPos = newPos
@@ -135,6 +188,7 @@ def MouseToKey(key):
                     yPos += movementSizestep
                 if yPos > centery:
                     yPos -= movementSizestep
+
             if xPos > xLeft:
                 newPos = xPos - movementSizestep
                 xPos = newPos
@@ -144,42 +198,9 @@ def MouseToKey(key):
                     yPos += movementSizestep
                 if yPos > centery:
                     yPos -= movementSizestep
+
             if xPos < xRight:
                 newPos = xPos + movementSizestep
-                xPos = newPos
-
-    # ____________________________Far Positions______________________________#
-        # because of my bad inital setup, of getting each needed key as a hot key
-        # This has to be done the long way. Ideally I would just have a HalfSize boolean that gets toggled if
-        # shift is pressed.
-        # If this gets any bigger, I will have to figure something out or do this in c++
-        case 'upshift':
-            # Order Matters
-            if yPos == yTop:
-                xPos = centerx
-            if yPos > yTop:
-                newPos = yPos - (movementSizestep*2)
-                yPos = newPos
-
-        case 'downshift':
-            if yPos == yBot:
-                xPos = centerx
-            if yPos < yBot:
-                newPos = yPos + (movementSizestep*2)
-                yPos = newPos
-
-        case 'leftshift':
-            if xPos == xLeft:
-                yPos = centery
-            if xPos > xLeft:
-                newPos = xPos - (movementSizestep*2)
-                xPos = newPos
-
-        case 'rightshift':
-            if xPos == xLeft:
-                yPos = centery
-            if xPos > xLeft:
-                newPos = xPos + (movementSizestep*2)
                 xPos = newPos
 
         case 'right alt':
@@ -203,18 +224,16 @@ if __name__ == '__main__':
     keyboard.add_hotkey('right', lambda: MouseToKey('right'),timeout=0)
     keyboard.add_hotkey('m', lambda: MouseToKey('right alt'),timeout=0)
 
+    #keyboard.add_hotkey('space', lambda: UpdateScreenshot(),timeout=0)
+
+    # Get Applies Autoaim for skills (against minions)
+    for item in inputKeys:
+        keyboard.add_hotkey(item, lambda: UpdateScreenshot(), timeout=0)
+
+
     # ____________________________Far Positions______________________________#
     # Makes Range smaller or bigger for ADC, vs Melee/.
     keyboard.add_hotkey('capslock',lambda: SwapRange())
-
-    # Use if you want a worse experience
-    '''
-    keyboard.add_hotkey('up+shift',lambda: MouseToKey('upshift'),timeout=0)
-    keyboard.add_hotkey('down+shift',lambda: MouseToKey('downshift'),timeout=0)
-
-    keyboard.add_hotkey('left+shift',lambda: MouseToKey('leftshift'),timeout=0)
-    keyboard.add_hotkey('right+shift',lambda: MouseToKey('rightshift'),timeout=0)
-    '''
 
 
     # I fucking hate this janky shitty module MAKE A BETTER THING omzomgroemageinrgaleijrgalerglahrelkaheroa
